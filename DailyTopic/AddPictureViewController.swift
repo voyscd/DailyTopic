70c6d432d2b1da3aa1971fe812f5425cbb95ba69
Daniel <1173143838@qq.com>
2016-05-04 22:57:40 +1000

    Finish the CamerFunction

--- DailyTopic/AddPictureViewController.swift
+++ DailyTopic/AddPictureViewController.swift
@@ -8,15 +8,45 @@
 
 import UIKit
 
-class AddPictureViewController: UIViewController,UIImagePickerControllerDelegate {
+class AddPictureViewController: UIViewController,UIImagePickerControllerDelegate,UINavigationControllerDelegate {
 
+    @IBOutlet weak var returnCreateTopic: UIBarButtonItem!
+    
+    var imagePicker = UIImagePickerController()
+    
+    @IBAction func returnCreateTopicButton(sender: AnyObject) {
+           self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
     @IBOutlet weak var imageView: UIImageView!
     
     @IBAction func cameraBtn(sender: UIBarButtonItem) {
+        
+        
+        imagePicker.sourceType = UIImagePickerControllerSourceType.Camera
+        presentViewController(imagePicker, animated: true, completion: nil)
     }
     
     @IBAction func libraryBtn(sender: AnyObject) {
+    
+        imagePicker.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+        presentViewController(imagePicker, animated: true, completion: nil)
     }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imageView.image = UIImage(named:"joker.jpg")
+        imagePicker.delegate = self
+        
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        imageView.image = info[UIImagePickerControllerOriginalImage] as? UIImage
+        
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
     
     
     

56206b61444936abb4a96ad0bd331c492ad74d94
Daniel <1173143838@qq.com>
2016-05-04 22:32:04 +1000

    Add Picture Version 1.0

---
+++ DailyTopic/AddPictureViewController.swift
@@ -0,0 +1,27 @@
+//
+//  AddPictureViewController.swift
+//  DailyTopic
+//
+//  Created by Daniel on 4/05/2016.
+//  Copyright © 2016 Daniel. All rights reserved.
+//
+
+import UIKit
+
+class AddPictureViewController: UIViewController,UIImagePickerControllerDelegate {
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBAction func cameraBtn(sender: UIBarButtonItem) {
+    }
+    
+    @IBAction func libraryBtn(sender: AnyObject) {
+    }
+    
+    
+    
+    
+    
+    
+    
+}

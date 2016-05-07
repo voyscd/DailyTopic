9c60cd5aec62bab75a16e30d10dc0c7da6d66bd5
Daniel <1173143838@qq.com>
2016-05-07 16:47:49 +1000

    Dispaly User name and Email in profile

--- DailyTopic/UserProfileViewController.swift
+++ DailyTopic/UserProfileViewController.swift
@@ -16,11 +16,37 @@ class UserProfileViewController: UIViewController {
     
     @IBOutlet weak var userNameLabel: UILabel!
     
+    @IBOutlet weak var userEmailLaabel: UILabel!
     
     override func viewDidLoad() {
          super.viewDidLoad()
         
-        var ref = Firebase(url: "https://docs-examples.firebaseio.com/web/saving-data/fireblog")
+        
+        //Get the User Information from the Firebase
+        
+        let authData = CURRENT_USER.authData.description.componentsSeparatedByString(" ")
+        
+        let uid = authData[1]
+        
+        var ref = Firebase(url:"https://dailytopic-daniel.firebaseio.com/DailyTopic/Users/" + uid)
+
+        print(uid)
+        
+        
+    
+        ref.observeEventType(.Value, withBlock: { snapshot in
+         
+        self.userNameLabel.text  = snapshot.value.objectForKey("UserName") as? String
+        self.userEmailLaabel.text = snapshot.value.objectForKey("UserEmail") as? String
+        
+          
+        },withCancelBlock: { error in
+            print(error.description)
+        })
+     
+        
+        
+        
          
     }
     

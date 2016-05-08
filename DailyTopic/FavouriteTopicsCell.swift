4b14612e542e6f8ba35d0462e138cf1bee4a725e
Daniel <1173143838@qq.com>
2016-05-08 20:11:17 +1000

    Desing Favourite layout

---
+++ DailyTopic/FavouriteTopicsCell.swift
@@ -0,0 +1,20 @@
+//
+//  FavouriteTopicsCell.swift
+//  DailyTopic
+//
+//  Created by Daniel on 8/05/2016.
+//  Copyright © 2016 Daniel. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class FavouriteTopicsCell: UITableViewCell {
+
+    
+    
+    
+    
+    
+    
+}
